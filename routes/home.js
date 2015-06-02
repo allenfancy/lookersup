b7c46e9221fb97786d9a0a73b7f9993f9191b464
HP <HP@HP-PC>
2015-06-02 16:19:42 +0800

    首页布局等部分展现

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,10 @@
 module.exports = function(app) {
 	console.log('come into home page');
 	app.get('/home', function(req, res) {
-		console.log('session.user.name ：　'+req.session.user.name);
+		//console.log('session.user.name ：　'+req.session.user.name);
 		res.render('home', {
 			title : '主页',
-			user:req.session.user
+			//user:req.session.user
 		});
 	});
 	
