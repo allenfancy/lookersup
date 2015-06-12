c6ea18c752dfce8cc1f339ede59f2400a79d76b1
fancy <371200745@qq.com>
2015-06-12 11:07:36 -0400

    配置线上的环境

--- common/config.js
+++ common/config.js
@@ -4,4 +4,4 @@ exports.email = 'allen_520_fancy@163.com';
 exports.session_secret = 'lookersup session secret';
 //本地数据库环境：  mongodb://127.0.0.1:27017/lookersup
 //部署的环境： mongodb://lookersup:wt6815876@ds047752.mongolab.com:47752/lookersup
-exports.db = 'mongodb://lookersup:wt6815876@ds047752.mongolab.com:47752/lookersup';
+exports.db = 'mongodb://<lookersup>:<wt6815876>@ds047752.mongolab.com:47752/lookersup'
