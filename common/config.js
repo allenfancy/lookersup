8ca5c905dd4e8ce67d499c6fa1ec72cca08d3396
HP <HP@HP-PC>
2015-06-12 18:22:13 +0800

    线上数据配置

--- common/config.js
+++ common/config.js
@@ -2,5 +2,6 @@ exports.dubug = true;
 exports.port = 3000;
 exports.email = 'allen_520_fancy@163.com';
 exports.session_secret = 'lookersup session secret';
-
-exports.db = 'mongodb://127.0.0.1:27017/lookersup';
+//本地数据库环境：  mongodb://127.0.0.1:27017/lookersup
+//部署的环境： mongodb://lookersup:wt6815876@ds047752.mongolab.com:47752/lookersup
+exports.db = 'mongodb://lookersup:wt6815876@ds047752.mongolab.com:47752/lookersup';
